6ee722d7342bf49ba234d7b18ddf971ade01c35f
emilio-devesa <92723521+emilio-devesa@users.noreply.github.com>
2025-03-06 10:42:30 +0100

    Code lintin

--- src/config.php
+++ src/config.php
@@ -1,9 +1,7 @@
-<?php/*
-	config.php
-	Configuration file
-*/?>
-
 <?php
+    // config.php
+	// Configuration file
+
     // MySQL variables
     $mysql_server = "localhost:8889";
     $mysql_username = "root";
